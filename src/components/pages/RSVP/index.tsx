2d895239161e57ec419e3c084bc9c3804a1947a9
Erik Ehresmann <erik.ehresmann9543@gmail.com>
2020-07-18 19:52:38 -0500

    front end commit

--- src/components/pages/RSVP/index.tsx
+++ src/components/pages/RSVP/index.tsx
@@ -3,17 +3,73 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import "./index.css";
+import axios from "axios";
+import "./index.css";  
+
+
+type Guest = {
+  guestName?: string,
+  plusOneName?: string,
+  children?: number,
+  guestFoodChoice?: string,
+  plusOneFoodChoice?: string,
+  bedsNeeded?: number
+}
+
 const RSVP = () => {
+
+const [guest, setGuest] = React.useState<Guest | null>(null);
+
+const changeGuestName = (e: Event | any) => {
+  setGuest({...guest, guestName: e.target.value})
+}
+
+const changePlusOneName = (e: Event | any) => {
+  setGuest({...guest, plusOneName: e.target.value})
+}
+
+const changeChildren = (e: Event | any) => {
+  setGuest({...guest, children: e.target.value})
+}
+
+const changeGuestFoodChoice = (e: Event | any) => {
+  setGuest({...guest, guestFoodChoice: e.target.value})
+} 
+
+const changePlusOneFoodChoice = (e: Event | any) => {
+  setGuest({...guest, plusOneFoodChoice: e.target.value})
+}
+
+const changeBedsNeeded = (e: Event | any) => {
+  setGuest({...guest, bedsNeeded: e.target.value})
+}
+
+
+const submitData = (e:any) => {
+  e.preventDefault();
+  axios.post("http://localhost:8080/weddingguestrsvp", guest, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(() => {
+    console.log("Made it here!")
+  })
+  .catch((err) => { 
+    console.log(err)
+  })
+}
+
+
   return (
     <div>
       <Col md={6}>
         <Card>
           <header className="headertitle">RSVP</header>
-          <Form>
+          <Form onSubmit={submitData}>
             <Form.Group className="name" controlId="Name">
               <Form.Label></Form.Label>
-              <Form.Control type="text" placeholder="Name" />
+              <Form.Control type="text" placeholder="Name" value={guest?.guestName} onChange={changeGuestName} />
             </Form.Group>
 
             <Form.Group className="plusone" controlId="PlusOneCheckBox">
@@ -22,7 +78,7 @@ const RSVP = () => {
 
             <Form.Group className="plusonename" controlId="PlusOneName">
               <Form.Label></Form.Label>
-              <Form.Control type="text" placeholder="Plus One's Name?" />
+              <Form.Control type="text" placeholder="Plus One's Name?" value={guest?.plusOneName} onChange={changePlusOneName} />
             </Form.Group>
 
             <Form.Group className="number" controlId="Children">
@@ -30,6 +86,7 @@ const RSVP = () => {
               <Form.Control
                 type="number"
                 placeholder="Number of Children You're Bringing?"
+                value={guest?.children} onChange={changeChildren}
               />
               <Form.Text className="text-muted">
                 Children Will Receive Chicken Fingers As Their Dinner.
@@ -43,12 +100,16 @@ const RSVP = () => {
                 label="Filet Mignon"
                 type={"radio"}
                 id={`inline-${"radio"}-1`}
+                value="Filet Mignon"
+                onClick={changeGuestFoodChoice}
               />
               <Form.Check
                 inline
                 label="Tuxedo Chicken"
                 type={"radio"}
                 id={`inline-${"radio"}-2`}
+                value="Tuxedo Chicken"
+                onClick={changeGuestFoodChoice}
               />
             </div>
 
@@ -77,10 +138,10 @@ const RSVP = () => {
 
             <Form.Group controlId="Children">
               <Form.Label></Form.Label>
-              <Form.Control type="number" placeholder="Number of Beds?" />
+              <Form.Control type="number" placeholder="Number of Beds?" value={guest?.bedsNeeded} onChange={changeBedsNeeded}/>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" onClick={submitData}>
               Submit
             </Button>
           </Form>
