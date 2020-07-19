8682d3e4fdd87b399c4900eabc0e468c0dc26749
Jackie <jackie.cychosz@knode.app>
2020-07-18 20:47:07 -0500

    html and css look prettier

--- src/components/pages/RSVP/index.tsx
+++ src/components/pages/RSVP/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./index.css";
+
 const RSVP = () => {
   return (
     <div>
       <Col md={6}>
-        <Card>
-          <header className="headertitle">RSVP</header>
-          <Form>
+          <Row>
+        <Card className="card">
+            <header className="headertitle">RSVP</header>
+            <Form>
             <Form.Group className="name" controlId="Name">
               <Form.Label></Form.Label>
               <Form.Control type="text" placeholder="Name" />
@@ -19,13 +22,13 @@ const RSVP = () => {
             <Form.Group className="plusone" controlId="PlusOneCheckBox">
               <Form.Check type="checkbox" label="Plus One?" />
             </Form.Group>
-
+            
             <Form.Group className="plusonename" controlId="PlusOneName">
               <Form.Label></Form.Label>
               <Form.Control type="text" placeholder="Plus One's Name?" />
             </Form.Group>
 
-            <Form.Group className="number" controlId="Children">
+            <Form.Group className="children" controlId="Children">
               <Form.Label></Form.Label>
               <Form.Control
                 type="number"
@@ -36,35 +39,47 @@ const RSVP = () => {
               </Form.Text>
             </Form.Group>
 
-            <div className="dinner">
-              <div>Your Dinner Choice?</div>
-              <Form.Check
+            <div>
+              <div className="dinner">Your Dinner Choice?</div>
+              <Form.Check className="filetmignon"
                 inline
                 label="Filet Mignon"
                 type={"radio"}
                 id={`inline-${"radio"}-1`}
               />
-              <Form.Check
+              <Form.Check className="tuxedochicken"
                 inline
                 label="Tuxedo Chicken"
                 type={"radio"}
                 id={`inline-${"radio"}-2`}
+              />
+               <Form.Check className="vegetable"
+                inline
+                label="Veggie Stack"
+                type={"radio"}
+                id={`inline-${"radio"}-3`}
               />
             </div>
 
             <div>
-              <div className="dinner">Your Plus One's Dinner Choice?</div>
-              <Form.Check className="dinner"
+              <div className="dinnertwo">Your Plus One's Dinner Choice?</div>
+              <Form.Check className="filetmignon"
                 inline
                 label="Filet Mignon"
                 type={"radio"}
                 id={`inline-${"radio"}-1`}
               />
-              <Form.Check className="dinner"
+              <Form.Check className="tuxedochicken"
                 inline
                 label="Tuxedo Chicken"
                 type={"radio"}
                 id={`inline-${"radio"}-2`}
+              />
+              <Form.Check className="vegetable"
+                inline
+                label="Veggie Stack"
+                type={"radio"}
+                id={`inline-${"radio"}-3`}
               />
             </div>
 
@@ -75,16 +90,19 @@ const RSVP = () => {
               />
             </Form.Group>
 
-            <Form.Group controlId="Children">
+            <Form.Group className="hotelbeds" controlId="Beds">
               <Form.Label></Form.Label>
               <Form.Control type="number" placeholder="Number of Beds?" />
             </Form.Group>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            
+            <div className="text-center">
+                <Button className="button" variant="outline-primary" type="submit">
+                Submit
+                </Button>
+            </div>
           </Form>
         </Card>
+        </Row>
       </Col>
       <Col></Col>
     </div>
