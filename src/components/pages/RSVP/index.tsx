508774c2ee61b2d89878c54427bc5e84482c8fae
Jackie <jackie.cychosz@knode.app>
2020-09-20 11:19:12 -0500

    fixed RSVP page

--- src/components/pages/RSVP/index.tsx
+++ src/components/pages/RSVP/index.tsx
@@ -6,11 +6,11 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./index.css";
 import axios from "axios";
-import {Reservation} from "../../../types/Reservation";
+import { Reservation } from "../../../types/Reservation";
 import GuestForm from "../../subcomponents/GuestForm";
-import {Guest} from "../../../types/Guest";
-import Modal from 'react-bootstrap/Modal'
-import Redirect from 'react-router-dom/Redirect'
+import { Guest } from "../../../types/Guest";
+import Modal from "react-bootstrap/Modal";
+import Redirect from "react-router-dom/Redirect";
 
 const RSVP = () => {
   const [show, setShow] = React.useState(false);
@@ -19,20 +19,22 @@ const RSVP = () => {
   const handleShow = () => setShow(true);
   const [guests, setGuests] = React.useState<Guest[]>([
     {
-      guestName: '',
-      guestFoodChoice: '',
+      guestName: "",
+      guestFoodChoice: "",
     },
-  ])
-  const [reservation, setReservation] = React.useState<Reservation | null>(null);
+  ]);
+  const [reservation, setReservation] = React.useState<Reservation | null>(
+    null
+  );
 
   const addGuest = (e: Event | any) => {
-    var newState:Guest[] = guests;
+    var newState: Guest[] = guests;
     newState.push({
-      guestName: '',
-      guestFoodChoice: '',
+      guestName: "",
+      guestFoodChoice: "",
     });
     setGuests([...newState]);
-  }
+  };
 
   const changeChildren = (e: Event | any) => {
     setReservation({ ...reservation, children: e.target.value });
@@ -40,27 +42,33 @@ const RSVP = () => {
 
   const submitData = (e: any) => {
     e.preventDefault();
-    setReservation({...reservation, guests: guests})
+    setReservation({ ...reservation, guests: guests });
     const payload = {
       name: guests[0].guestName,
       guest: {
         ...guests,
-        ...reservation
-      }
-    }
+        ...reservation,
+      },
+    };
     axios
-      .post("https://us-central1-ethanandjackie-f71f0.cloudfunctions.net/app/api/create", payload, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
+      .post(
+        "https://us-central1-ethanandjackie-f71f0.cloudfunctions.net/app/api/create",
+        payload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
       .then(() => {
-        setGuests( [{
-          guestName: '',
-          guestFoodChoice: '',
-        }]);
+        setGuests([
+          {
+            guestName: "",
+            guestFoodChoice: "",
+          },
+        ]);
         handleShow();
-        setTimeout(function(){
+        setTimeout(function () {
           setSubmitted(true);
         }, 3000);
       })
@@ -76,31 +84,57 @@ const RSVP = () => {
           <Card className="card">
             <header className="headertitle">RSVP</header>
             <Form onSubmit={submitData}>
-
-
-              { guests.map((guest, index) => {
-                return(
-                    <div>
-                      <GuestForm index={index} guests={guests} guest={guests[index]} setGuest={setGuests} />
-                    </div>
-                    )
-              })}              
-              <Form.Label className="children">Add People In Your Party (Plus 1s And Children Aged 12+)</Form.Label>
-              <Button size="sm" id="addperson" onClick={addGuest} variant="outline-primary">+</Button>{' '}
-
+              {guests.map((guest, index) => {
+                return (
+                  <div>
+                    <GuestForm
+                      index={index}
+                      guests={guests}
+                      guest={guests[index]}
+                      setGuest={setGuests}
+                    />
+                  </div>
+                );
+              })}
+              <Row>
+                <Col>
+                  <Form.Label className="children">
+                    Add People In Your Party (Plus 1s And Children Aged 12+)
+                  </Form.Label>
+                </Col>
+                <Col>
+                  <Button
+                    size="sm"
+                    id="addperson"
+                    onClick={addGuest}
+                    variant="outline-primary"
+                  >
+                    +
+                  </Button>{" "}
+                </Col>
+              </Row>
               <Form.Group className="children" controlId="Children">
-                <Form.Label> Bringing Young Children?</Form.Label>
-                <Form.Control
-                  max="5"
-                  min="0"
-                  type="number"
-                  placeholder="Number of Young Children (Aged 0-11) You're Bringing?"
-                  value={reservation?.children}
-                  onChange={changeChildren}
-                />
-                <Form.Text className="text-muted">
-                  Young Children Will Receive Chicken Fingers As Their Dinner.
-                </Form.Text>
+                <Row>
+                  <Col>
+                    <Form.Label>
+                      {" "}
+                      Number of Young Children You're Bringing?
+                    </Form.Label>
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      max="5"
+                      min="0"
+                      type="number"
+                      value={reservation?.children}
+                      onChange={changeChildren}
+                    />
+                    <Form.Text className="text-muted">
+                      Young Children Will Receive Chicken Fingers As Their
+                      Dinner.
+                    </Form.Text>
+                  </Col>
+                </Row>
               </Form.Group>
               <Button
                 id="button-group"
@@ -116,7 +150,7 @@ const RSVP = () => {
       </Col>
       <Col xs={0} sm={0} md={0} lg={6}></Col>
       <Modal show={show}>
-        <Modal.Header >
+        <Modal.Header>
           <Modal.Title>RSVP Sent!</Modal.Title>
         </Modal.Header>
         <Modal.Footer>
@@ -125,7 +159,7 @@ const RSVP = () => {
           </Button>
         </Modal.Footer>
       </Modal>
-      {submitted && <Redirect to="/info" /> }
+      {submitted && <Redirect to="/info" />}
     </div>
   );
 };
--- src/components/subcomponents/GuestForm/index.tsx
+++ src/components/subcomponents/GuestForm/index.tsx
@@ -33,10 +33,10 @@ const GuestForm = (props: any) => {
 
   const removeGuest = () => {
     var newState: Guest[] = props.guests;
-    const index = newState.indexOf(props.guest)
+    const index = newState.indexOf(props.guest);
     newState.splice(index, 1);
     props.setGuest([...newState]);
-  }
+  };
 
   return (
     <div>
@@ -44,52 +44,67 @@ const GuestForm = (props: any) => {
         <Form>
           <div className="rowchange">
             <Row>
-              <Col lg={11}>
+              <Col lg={11}></Col>
+              <Col lg={1}>
+                <Button
+                  size="sm"
+                  onClick={removeGuest}
+                  variant="light"
+                  id="cancel"
+                >
+                  x
+                </Button>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
                 <Form.Group className="name" controlId="Name">
                   <Form.Label>Name</Form.Label>
                   <Form.Control
                     type="text"
-                    placeholder="Name"
                     value={props.guest.guestName}
                     onChange={changeGuestName}
                   />
                 </Form.Group>
               </Col>
-              <Col lg={1}>
-                <Button size="sm" onClick={removeGuest} variant="light" id="cancel">x</Button>
-              </Col>
             </Row>
           </div>
           <div>
-            <div className="dinner">Dinner Selection?</div>
-            <Form.Check
-              name="dinner"
-              className="filetmignon"
-              inline
-              label="Filet Mignon"
-              type={"radio"}
-              id={`inline-${"radio"}-1`}
-              value="Filet Mignon"
-              onClick={changeGuestFoodChoice}
-            />
-            <Form.Check
-              name="dinner"
-              className="tuxedochicken"
-              inline
-              label="Tuxedo Chicken"
-              type={"radio"}
-              id={`inline-${"radio"}-2`}
-              value="Tuxedo Chicken"
-              onClick={changeGuestFoodChoice}
-            />
-            <Form.Check
-              name="dinner"
-              className="veggie"
-              inline
-              label="Veggie Stack"
-              type={"radio"}
-              id={`inline-${"radio"}-3`}
-            />
+            <Row>
+              <Col>
+                <div className="dinner">Dinner Selection?</div>
+              </Col>
+              <Col>
+                <Form.Check
+                  name="dinner"
+                  className="filetmignon"
+                  inline
+                  label="Filet Mignon"
+                  type={"radio"}
+                  id={`inline-${"radio"}-1`}
+                  value="Filet Mignon"
+                  onClick={changeGuestFoodChoice}
+                />
+                <Form.Check
+                  name="dinner"
+                  className="tuxedochicken"
+                  inline
+                  label="Tuxedo Chicken"
+                  type={"radio"}
+                  id={`inline-${"radio"}-2`}
+                  value="Tuxedo Chicken"
+                  onClick={changeGuestFoodChoice}
+                />
+                <Form.Check
+                  name="dinner"
+                  className="veggie"
+                  inline
+                  label="Veggie Stack"
+                  type={"radio"}
+                  id={`inline-${"radio"}-3`}
+                />
+              </Col>
+            </Row>
           </div>
         </Form>
       </Card>
